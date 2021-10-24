2c8fc2e6732fa4633ab37bc87e1c4b39a6230d6a
adaruiz <ada.ruiz93@gmail.com>
2021-10-23 23:18:19 -0700

    added routes to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Navbar from './components/PortfolioNavbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Projects from './components/Projects'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
 
 function App() {
   return (
     <div>
      <Navbar />
+     <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/resume' component={Resume} />
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/PortfolioNavbar.js
+++ src/components/PortfolioNavbar.js
@@ -1,26 +1,44 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Nav, Navbar, Container, ButtonGroup } from 'react-bootstrap'
+import { Nav, Navbar, Container } from 'react-bootstrap'
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Projects from './Projects'
+import About from './About'
+import Contact from './Contact'
+import Resume from './Resume'
+
 
 function PortfolioNavbar() {
   return (
+    <Router>
     <div>
     <Navbar fixed="top" bg="myNav" variant="light" expand="lg">
     <Container>
-      <Navbar.Brand href="/">Ada Ruiz &hearts;</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-nav-bar-nav" />
+      <Navbar.Brand as={Link} to="/">Ada Ruiz &hearts;</Navbar.Brand>
+      <NavbarToggle aria-controls="basic-nav-bar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-         <Nav.Link href="/projects">Projects</Nav.Link>
-         <Nav.Link href="/about">About</Nav.Link>
-         <Nav.Link href="/contact">Contact</Nav.Link>
-         <Nav.Link href="/resume">Resume</Nav.Link>
+         <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+         <Nav.Link as={Link} to="/about">About</Nav.Link>
+         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+         <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
         </Nav> 
       </Navbar.Collapse>
     </Container>
   </Navbar>
     </div>
+    <div>
+      <Switch>
+        <Route path="/"><Home /></Route>
+        <Route path="/projects"><Projects /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/contact"><Contact /></Route>
+        <Route path="/resume"><Resume /></Route>
+      </Switch>
+    </div>
+    </Router>
   )
 }
 
